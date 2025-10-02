6149eb38a329a8a3806679a69bb711d6bf8317bd
Perman-developer <permanshamuratov@gmail.com>
2025-10-02 19:13:52 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-BOT_TOKEN = "8050417005:AAGTsf9ALzLWC8hfU-WhyMMh6sjLdqNiHQI"
+BOT_TOKEN = "8050417005:AAGTsf9ALzLWC8hfU-WhyMMsjLdqNiHQI"
 
 DB_NAME = "database/users.db"
 
-ADMIN_IDS = [7547413961, 7642124836]
-SUPER_ADMIN = 7547413961
+ADMIN_IDS = [754741361, 764212486]
+SUPER_ADMIN = 75474196
 
 referal_price = 100
-FIO = "Shamuratov Perman"
+FIO = "Palonchiyev palonchi"
 karta = "8600 0000 0000 0000"
 min_pay = 1000
 max_pay = 1000000
 
-API_KEY = "4c5077fe42de13a9f853956a9ee9c2b0"  # <-- O'z API kalitingizni qo'ying
+API_KEY = "4c5077fe42de13a9f853959ee9c2b0"  # <-- O'z API kalitingizni qo'ying
 API_URL = "https://topsmm.uz/api/v2"
 
 ORDER_DB = "database/orders.db"
